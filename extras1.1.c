76a2f057b8b2201d398f00175adfe1347a59cf72
Sagar Joshi <91314367+leoemn@users.noreply.github.com>
2023-08-14 19:26:33 +0100

    Add files via upload

---
+++ extras1.1.c
@@ -0,0 +1,20 @@
+/*Write a program that allocates memory for a single integer using `malloc` and sets its value to 44. 
+Print the value.*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    int *a = malloc(sizeof(int));
+
+    if (a == NULL)
+    {
+        printf("memory allocation failed");
+        return 1;
+    }
+
+    *a = 44;
+    printf("%d", *a);
+    free(a);
+}
