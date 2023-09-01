c5fcdfc9d20096e087d666d33e74397d1b924411
Sagar Joshi <91314367+leoemn@users.noreply.github.com>
2023-09-01 18:23:26 +0100

    Add files via upload

---
+++ extras2.6.c
@@ -0,0 +1,31 @@
+/*Write a program to merge two sorted linked lists into a single sorted list.*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+}node;
+
+int main(void)
+{
+    node *link1 = NULL;
+    node *link2 = NULL;
+
+    //Creating first linked lists with 5 elements in it.
+    for (int i = 0 ; i < 5 ; i++)
+    {
+        int number;
+        printf("Enter the element: ");
+        scanf("%d", &number);
+
+        node *NewNode = malloc(sizeof(node));
+
+        NewNode -> data = number;
+        NewNode -> next = link1;
+        link1 = NewNode;
+    }
+
+}
