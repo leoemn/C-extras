af03c5f447a39762962ab949a7ee4faa31f62825
Sagar Joshi <91314367+leoemn@users.noreply.github.com>
2023-09-04 13:17:43 +0100

    Add files via upload

--- extras2.6.c
+++ extras2.6.c
@@ -1,5 +1,3 @@
-/*Write a program to merge two sorted linked lists into a single sorted list.*/
-
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -9,25 +7,99 @@ typedef struct node
     struct node *next;
 }node;
 
+node* SortList(node *link1, node *link2);
+void PrintList(node *head);
+void InsertSorted(node **head, int data);
+
 int main(void)
 {
     node *link1 = NULL;
     node *link2 = NULL;
 
-    //Creating first linked lists with 5 elements in it.
+    //Creating linked lists with 5 elements in it.
     for (int i = 0 ; i < 5 ; i++)
     {
-        int number;
-        printf("Enter the element: ");
-        scanf("%d", &number);
+        int number1, number2;
+        printf("Enter the element for first linked list: ");
+        scanf("%d", &number1);
+        InsertSorted(&link1, number1);
 
-        node *NewNode = malloc(sizeof(node));
-
-        NewNode -> data = number;
-        NewNode -> next = link1;
-        link1 = NewNode;
+        printf("Enter the element for second linked list: ");
+        scanf("%d", &number2);
+        InsertSorted(&link2, number2);
     }
-    
-    for (int i = 0 ; i < 5 ; i++)
 
+    node *MergedList = SortList(link1, link2);
+    printf("Merged and Sorted List: \n");
+    PrintList(MergedList);
+
+    // Freeing up allocated memory
+    while (MergedList != NULL)
+    {
+        node *temp = MergedList;
+        MergedList = MergedList->next;
+        free(temp);
+    }
+
+    return 0;
+}
+
+void InsertSorted(node **head, int data)
+{
+    node *NewNode = malloc(sizeof(node));
+    NewNode->data = data;
+    NewNode->next = NULL;
+
+    if (*head == NULL || (*head)->data >= NewNode->data)
+    {
+        NewNode->next = *head;
+        *head = NewNode;
+    }
+    else
+    {
+        node *current = *head;
+        while (current->next != NULL && current->next->data < NewNode->data)
+        {
+            current = current->next;
+        }
+        NewNode->next = current->next;
+        current->next = NewNode;
+    }
+}
+
+node* SortList(node *link1, node *link2)
+{
+    node *merged = NULL, **lastPtr = &merged;
+
+    while (link1 && link2)
+    {
+        if (link1->data < link2->data)
+        {
+            *lastPtr = link1;
+            link1 = link1->next;
+        }
+        else
+        {
+            *lastPtr = link2;
+            link2 = link2->next;
+        }
+        lastPtr = &((*lastPtr)->next);
+    }
+
+    if (link1) 
+        *lastPtr = link1;
+    else 
+        *lastPtr = link2;
+
+    return merged;
+}
+
+void PrintList(node *head)
+{
+    while (head != NULL)
+    {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
 }
